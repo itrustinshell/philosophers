4ebcf22a0b3745e72a414b5ce2ee7d47443dd47c
Luigi Argenziano Vagello <largenzi@e3r9p1.42roma.it>
2024-11-03 12:41:24 +0100

    memopry leak ok

--- philosophers/src/free.c
+++ philosophers/src/free.c
@@ -6,20 +6,21 @@
 /*   By: largenzi <largenzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 17:50:25 by largenzi          #+#    #+#             */
-/*   Updated: 2024/11/03 10:21:54 by largenzi         ###   ########.fr       */
+/*   Updated: 2024/11/03 12:38:10 by largenzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 void	free_everything(t_fork *forks_array,
-		t_thread_pmt *pmt_array, pthread_t *philosophers)
+		t_thread_pmt *pmtarray, pthread_t *philosophers)
 {
-	free(pmt_array->mutexwrite);
-	free(pmt_array->someonediedptr);
-	free(pmt_array->finishprintptr);
-	free(pmt_array->mutexdeath);
-	free(pmt_array);
+	free(pmtarray->mutexdeath);
+	free(pmtarray->mutexwrite);
+	free(pmtarray->mutexfinishprint);
+	free(pmtarray->someonediedptr);
+	free(pmtarray->finishprintptr);
+	free(pmtarray);
 	free(forks_array);
 	free(philosophers);
 }
